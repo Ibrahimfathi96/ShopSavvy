5a123cc9fe8806a11b06c626b137cec067f671c3
Ibrahim Fathi <himafathy796@gmail.com>
2023-06-09 22:57:41 +0300

    [FEATURE] Home UI part 5

--- lib/controller/home/home_screen_controller.dart
+++ lib/controller/home/home_screen_controller.dart
@@ -1,13 +1,14 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
 import 'package:shop_savvy/core/constants/color.dart';
 import 'package:shop_savvy/view/screen/home/home_body.dart';
 
-abstract class HomeScreenController extends GetxController{}
-class HomeScreenControllerImp extends HomeScreenController{
+abstract class HomeScreenController extends GetxController {}
+
+class HomeScreenControllerImp extends HomeScreenController {
   PersistentTabController persistentTabController = PersistentTabController();
+
   List<Widget> buildScreens() {
     return [
       const HomeBody(),
@@ -29,39 +30,54 @@ class HomeScreenControllerImp extends HomeScreenController{
   List<PersistentBottomNavBarItem> navBarsItems() {
     return [
       PersistentBottomNavBarItem(
-        icon: const Icon(Icons.home_outlined,size: 30,),
+        icon: const Icon(
+          Icons.home_outlined,
+          size: 30,
+        ),
         title: "Home",
         textStyle: const TextStyle(fontSize: 18),
         activeColorPrimary: AppColors.primaryColor,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
+        inactiveColorPrimary: Colors.grey[700],
       ),
       PersistentBottomNavBarItem(
-        icon: const Icon(Icons.favorite_border,size: 30,),
+        icon: const Icon(
+          Icons.favorite_border,
+          size: 30,
+        ),
         title: "Favorite",
         textStyle: const TextStyle(fontSize: 18),
         activeColorPrimary: AppColors.primaryColor,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
+        inactiveColorPrimary: Colors.grey[700],
       ),
       PersistentBottomNavBarItem(
-        icon: const Icon(Icons.shopping_bag_outlined,size: 30,),
+        icon: const Icon(
+          Icons.shopping_bag_outlined,
+          size: 30,
+        ),
         title: "Cart",
         textStyle: const TextStyle(fontSize: 18),
         activeColorPrimary: AppColors.primaryColor,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
+        inactiveColorPrimary: Colors.grey[700],
       ),
       PersistentBottomNavBarItem(
-        icon: const Icon(CupertinoIcons.profile_circled,size: 30,),
+        icon: const Icon(
+          Icons.person_pin_outlined,
+          size: 30,
+        ),
         title: "Profile",
         textStyle: const TextStyle(fontSize: 18),
         activeColorPrimary: AppColors.primaryColor,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
+        inactiveColorPrimary: Colors.grey[700],
       ),
       PersistentBottomNavBarItem(
-        icon: const Icon(Icons.menu_outlined,size: 30,),
-        title: "More",
+        icon: const Icon(
+          Icons.settings_outlined,
+          size: 30,
+        ),
+        title: "Settings",
         textStyle: const TextStyle(fontSize: 18),
         activeColorPrimary: AppColors.primaryColor,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
+        inactiveColorPrimary: Colors.grey[700],
       ),
     ];
   }
--- lib/view/screen/home/home_screen.dart
+++ lib/view/screen/home/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
 import 'package:shop_savvy/controller/home/home_screen_controller.dart';
+import 'package:shop_savvy/view/screen/home/widgets/home_bottom_nav_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = '/home-screen';
@@ -17,44 +17,10 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     Get.put(HomeScreenControllerImp());
     return GetBuilder<HomeScreenControllerImp>(
-      builder: (controller) => Scaffold(
-        body: PersistentTabView(
-          context,
-          controller: controller.persistentTabController,
-          screens: controller.buildScreens(),
-          items: controller.navBarsItems(),
-          confineInSafeArea: true,
-          backgroundColor: Colors.white,
-          // Default is Colors.white.
-          handleAndroidBackButtonPress: true,
-          // Default is true.
-          resizeToAvoidBottomInset: true,
-          // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
-          stateManagement: true,
-          // Default is true.
-          hideNavigationBarWhenKeyboardShows: true,
-          // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
-          decoration: NavBarDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            colorBehindNavBar: Colors.white,
-          ),
-          popAllScreensOnTapOfSelectedTab: true,
-          popActionScreens: PopActionScreensType.all,
-          itemAnimationProperties: const ItemAnimationProperties(
-            // Navigation Bar's items animation properties.
-            duration: Duration(milliseconds: 200),
-            curve: Curves.easeInOut,
-          ),
-          screenTransitionAnimation: const ScreenTransitionAnimation(
-            // Screen transition animation on change of selected tab.
-            animateTabTransition: true,
-            curve: Curves.ease,
-            duration: Duration(milliseconds: 200),
-          ),
-          navBarStyle: NavBarStyle
-              .style9, // Choose the nav bar style with this property.
-        ),
+      builder: (controller) => const Scaffold(
+        body: HomeBottomNavBar(),
       ),
     );
   }
 }
+
