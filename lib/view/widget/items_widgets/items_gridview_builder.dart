e26b6d3c1fb665ec169442280b45bdac69078818
Ibrahim Fathi <himafathy796@gmail.com>
2023-06-10 17:52:00 +0300

    [FEATURE] Items View retouch

--- lib/view/widget/items_widgets/items_gridview_builder.dart
+++ lib/view/widget/items_widgets/items_gridview_builder.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
 import 'package:shop_savvy/core/constants/color.dart';
 import 'package:shop_savvy/data/model/items_model.dart';
 import 'package:shop_savvy/link_api.dart';
@@ -19,35 +20,52 @@ class ItemsGridViewBuilder extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(8),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Image.network('${AppLink.laptops}/${itemsMd.itemsImage}'),
-              Text(
-                "${itemsMd.itemsName}",
-                textAlign: TextAlign.center,
-                style: const TextStyle(color: Colors.black87),
+              SizedBox(
+                height: 120,
+                child: Image.network(
+                  '${AppLink.laptops}/${itemsMd.itemsImage}',
+                  fit: BoxFit.fill,
+                ),
               ),
-              Text(
-                "${itemsMd.itemsDesc}",
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.grey[700],fontSize: 13),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0,horizontal: 2),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+              const SizedBox(height: 6,),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      "${itemsMd.itemsPrice} EGP",
-                      style: const TextStyle(color: AppColors.secondaryColor),
+                    SizedBox(
+                      height: 20,
+                      child: Marquee(
+                        velocity: 50,
+                        text:
+                          "${itemsMd.itemsName}    ",
+                          style: const TextStyle(color: Colors.black87,fontSize: 16),
+                      ),
                     ),
-                    const Spacer(),
-                    GestureDetector(
-                      onTap: (){},
-                      child: const Icon(
-                        Icons.favorite_border_outlined,
-                        color: AppColors.primaryColor,
+                    Text(
+                      "${itemsMd.itemsDesc}",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.grey[700], fontSize: 13),
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.symmetric(vertical: 8.0, horizontal: 2),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "${itemsMd.itemsPrice} EGP",
+                            style: const TextStyle(color: AppColors.secondaryColor),
+                          ),
+                          const Spacer(),
+                          GestureDetector(
+                            onTap: () {},
+                            child: const Icon(
+                              Icons.favorite_border_outlined,
+                              color: AppColors.primaryColor,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
