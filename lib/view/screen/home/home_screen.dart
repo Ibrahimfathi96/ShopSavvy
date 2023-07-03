a0260135c3ce630bbb9429cb9ddc8824e6e15ad5
Ibrahim Fathi <himafathy796@gmail.com>
2023-07-03 21:33:49 +0300

    [FEATURE] exit app alert

--- lib/view/screen/home/home_screen.dart
+++ lib/view/screen/home/home_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shop_savvy/controller/home_controllers/home_screen_controller.dart';
+import 'package:shop_savvy/core/constants/color.dart';
 import 'package:shop_savvy/view/widget/home_widgets/home_bottom_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -15,7 +18,30 @@ class HomeScreen extends StatelessWidget {
       builder: (controller) => Scaffold(
         bottomNavigationBar: const CustomHomeBottomAppBar(),
         body: SafeArea(
-          child: controller.pagesList[controller.currentPage]
+          child: WillPopScope(
+            onWillPop: () {
+              Get.defaultDialog(
+                title: "Alert!",
+                titleStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: AppColors.secondaryColor,
+                ),
+                middleText: "Do you want to exit the application?",
+                middleTextStyle: TextStyle(
+                  fontSize: 16,
+                  color: AppColors.primaryDark,
+                ),
+                onConfirm: () => exit(0),
+                buttonColor: AppColors.secondaryColor,
+                confirmTextColor: Colors.white,
+                cancelTextColor: AppColors.primaryDark,
+                onCancel: () {},
+              );
+              return Future.value(false);
+            },
+            child: controller.pagesList[controller.currentPage],
+          ),
         ),
       ),
     );
