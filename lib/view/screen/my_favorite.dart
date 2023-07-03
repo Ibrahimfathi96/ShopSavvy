58a821f8e9741d2ea08e3721553f3bd978bb2cc4
Ibrahim Fathi <himafathy796@gmail.com>
2023-07-03 20:33:14 +0300

    [FEATURE] offers Controller and UI

--- lib/view/screen/my_favorite.dart
+++ lib/view/screen/my_favorite.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:shop_savvy/controller/my_favorite_controller.dart';
 import 'package:shop_savvy/core/class/handling_data_view.dart';
 import 'package:shop_savvy/view/widget/home_widgets/custom_appbar.dart';
-import 'package:shop_savvy/view/widget/home_widgets/home_notification_icon.dart';
 import 'package:shop_savvy/view/widget/home_widgets/search_items_listview.dart';
 import 'package:shop_savvy/view/widget/my_favorite_widgets/custom_favorite_list.dart';
 
@@ -19,67 +18,64 @@ class MyFavoriteView extends StatelessWidget {
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.symmetric(horizontal: 12),
-          child: GetBuilder<MyFavoriteController>(builder: (controller) {
-            return Column(
-              children: [
-                CustomAppBar(
-                  onClosePress: () {
-                    controller.clearController(context);
-                  },
-                  myController: controller.searchController,
-                  onChanged: (val) {
-                    controller.checkSearching(val);
-                  },
-                  backButton: GestureDetector(
-                    onTap: () {
-                      controller.goBack();
+          child: GetBuilder<MyFavoriteController>(
+            builder: (controller) {
+              return Column(
+                children: [
+                  CustomAppBar(
+                    onClosePress: () {
+                      controller.clearController(context);
                     },
-                    child: const Icon(Icons.arrow_back_ios),
-                  ),
-                  favouriteButton: HomeAppBarIcons(
-                    onPressed: () {
-                      Get.toNamed(MyFavoriteView.routeName);
+                    myController: controller.searchController,
+                    onChanged: (val) {
+                      controller.checkSearching(val);
                     },
-                    iconData: Icons.favorite_border_outlined,
+                    backButton: GestureDetector(
+                      onTap: () {
+                        controller.goBack();
+                      },
+                      child: const Icon(Icons.arrow_back_ios),
+                    ),
+                    appBarTitle: "Find your product..",
+                    onSearchPress: () {
+                      controller.onItemsSearching();
+                    },
+                    onNotificationPress: () {},
                   ),
-                  appBarTitle: "Find your product..",
-                  onSearchPress: () {
-                    controller.onItemsSearching();
-                  },
-                  onNotificationPress: () {},
-                ),
-                controller.searching == false
-                    ? Expanded(
-                        child: ListView(
-                          physics: const BouncingScrollPhysics(),
-                          children: [
-                            HandlingDataView(
-                              statusRequest: controller.statusRequest,
-                              widget: GridView.builder(
-                                shrinkWrap: true,
-                                physics: const NeverScrollableScrollPhysics(),
-                                gridDelegate:
-                                    const SliverGridDelegateWithFixedCrossAxisCount(
-                                        crossAxisCount: 2,
-                                        childAspectRatio: 0.7,
-                                        crossAxisSpacing: 2),
-                                itemCount: controller.data.length,
-                                itemBuilder: (context, index) {
-                                  return FavoriteItemsGridViewBuilder(
-                                    itemsMd: controller.data[index],
-                                  );
-                                },
-                              ),
-                            )
-                          ],
+                  controller.searching == false
+                      ? Expanded(
+                          child: ListView(
+                            physics: const BouncingScrollPhysics(),
+                            children: [
+                              HandlingDataView(
+                                statusRequest: controller.statusRequest,
+                                widget: GridView.builder(
+                                  shrinkWrap: true,
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  gridDelegate:
+                                      const SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    childAspectRatio: 0.7,
+                                    crossAxisSpacing: 2,
+                                  ),
+                                  itemCount: controller.data.length,
+                                  itemBuilder: (context, index) {
+                                    return FavoriteItemsGridViewBuilder(
+                                      itemsMd: controller.data[index],
+                                    );
+                                  },
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      : ItemsListSearch(
+                          searchDataList: controller.searchList,
                         ),
-                      )
-                    : ItemsListSearch(
-                        searchDataList: controller.searchList,
-                      ),
-              ],
-            );
-          }),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
