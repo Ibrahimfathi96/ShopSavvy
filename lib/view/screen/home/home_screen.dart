7f815fb8f1322faadbb13e660d2a181590d34e62
Ibrahim Fathi <himafathy796@gmail.com>
2023-07-05 20:09:05 +0300

    [FEATURE] final retouches

--- lib/view/screen/home/home_screen.dart
+++ lib/view/screen/home/home_screen.dart
@@ -1,9 +1,7 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shop_savvy/controller/home_controllers/home_screen_controller.dart';
-import 'package:shop_savvy/core/constants/color.dart';
+import 'package:shop_savvy/core/functions/exit_app_alert.dart';
 import 'package:shop_savvy/view/widget/home_widgets/home_bottom_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -19,27 +17,7 @@ class HomeScreen extends StatelessWidget {
         bottomNavigationBar: const CustomHomeBottomAppBar(),
         body: SafeArea(
           child: WillPopScope(
-            onWillPop: () {
-              Get.defaultDialog(
-                title: "Alert!",
-                titleStyle: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                  color: AppColors.secondaryColor,
-                ),
-                middleText: "Do you want to exit the application?",
-                middleTextStyle: TextStyle(
-                  fontSize: 16,
-                  color: AppColors.primaryDark,
-                ),
-                onConfirm: () => exit(0),
-                buttonColor: AppColors.secondaryColor,
-                confirmTextColor: Colors.white,
-                cancelTextColor: AppColors.primaryDark,
-                onCancel: () {},
-              );
-              return Future.value(false);
-            },
+            onWillPop: exitAppAlertDialog,
             child: controller.pagesList[controller.currentPage],
           ),
         ),
